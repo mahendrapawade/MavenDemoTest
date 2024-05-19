4d9d56443ded0bcdcf760cb79b01f8dc77a096af
mahendra <mahendrapawde@gmail.com>
2024-05-19 09:53:34 +0530

    This is my first commit

---
+++ src/test/java/com/qa/tests/DemoTest2.java
@@ -0,0 +1,44 @@
+ package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 
+{
+	@Test
+	public void sum()
+	{
+	int a=10;
+	int b=20;
+	Assert.assertEquals(30, a+b);
+	}
+
+	@Test
+	public void sub()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+
+	@Test
+	public void div()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+		
+	}
+
+	@Test
+	public void mul()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+		
+	}
+
+	
+	
+}
